166ff1c2a3becb07f174f95dfbdd4294706130d3
Tejarsha <arigilatejarsha@gmail.com>
2025-04-18 23:33:21 -0500

    Initial commit for Jenkins test app

---
+++ src/main/java/com/example/jenkins_test_app/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_test_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome To Jenkins - v1";
+    }
+}
---
+++ src/test/java/com/example/jenkins_test_app/JenkinsTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_test_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
